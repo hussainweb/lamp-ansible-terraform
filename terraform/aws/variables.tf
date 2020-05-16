2ec968889c1869db66bc0866d7aed9b7be308bde
hw <hussainweb@gmail.com>
2020-05-16 19:34:16 -0400

    Remove unnecessary variables

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,5 +1,3 @@
-variable "access_key" {}
-variable "secret_key" {}
 variable "region" {
   default = "eu-west-1"
 }
@@ -7,29 +5,3 @@ variable "region" {
 variable "web_instance_type" {
   default = "t2.micro"
 }
-//
-//variable "name_prefix" {
-//  default = "dx-cms-webdev-"
-//}
-//variable "site_name" {}
-//variable "environment" {}
-//
-//variable "files_dir" {
-//  default = ""
-//}
-//
-//variable "drupal_base_protocol" {
-//  default = "http"
-//}
-//variable "drupal_base_domain_name" {}
-//
-//variable "drupal_acep_key" {}
-//
-//variable "drupal_api_proxy_lambda_zip" {
-//  default = ""
-//}
-//
-//locals {
-//  drupal_base_url = "${var.drupal_base_protocol}://${var.drupal_base_domain_name}"
-//  is_production = "${var.environment == "prod"}"
-//}
