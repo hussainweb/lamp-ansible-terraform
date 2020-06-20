1eae6c90866e299415266479874de2c33e458716
hussainweb <hussainweb@gmail.com>
2020-06-19 23:01:15 -0400

    Fix linting errors in generate-keys.sh

--- private-keys/generate-keys.sh
+++ private-keys/generate-keys.sh
@@ -3,4 +3,4 @@
 key_file_name=${1:-'server_key'}
 key_comment=${2:-'Server'}
 
-ssh-keygen -t rsa -b 4096 -f $key_file_name -N "" -C "$key_comment"
+ssh-keygen -t rsa -b 4096 -f "$key_file_name" -N "" -C "$key_comment"
